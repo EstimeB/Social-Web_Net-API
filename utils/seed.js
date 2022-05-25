079e2ca7ef935cc42dbc6d29cf3d8f48b03efc27
brith <bgc@briths-MacBook-Air.local>
2022-05-24 19:21:11 -0700

    Filled out seed.js.

---
+++ utils/seed.js
@@ -0,0 +1,36 @@
+// Importing connection, models, and data
+const connection = require('../config/connection');
+const { User, Thought } = require('../models');
+const { getRandomName, getRandomThoughts } = require('./data');
+
+// To pick up error in the callback (mongoose err when trying to connect to mongo DB)
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+  console.log('connected');
+  await Thought.deleteMany({});
+  await User.deleteMany({});
+
+  const users = [];
+  const thoughts = getRandomThoughts(10);
+
+  for (let i = 0; i < 20; i++) {
+    const fullName = getRandomName();
+    const first = fullName.split(' ')[0];
+    const last = fullName.split(' ')[1];
+
+    users.push({
+      first,
+      last,
+    });
+  }
+
+  await User.collection.insertMany(users);
+  await Thought.collection.insertMany(thoughts);
+
+  // loop through the saved applications, for each application we need to generate a application response and insert the application responses
+  console.table(users);
+  console.table(thoughts);
+  console.info('Seeding complete! 🌱');
+  process.exit(0);
+});
